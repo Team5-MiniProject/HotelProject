1c27547531668a3e9863be0545a1672408a94a22
Shinny <siniya979@gmail.com>
2022-11-25 17:20:25 +0900

    인터페이스 예약하기 수정

--- src/main/java/hotelBookingProgram/HotelBookingList.java
+++ src/main/java/hotelBookingProgram/HotelBookingList.java
@@ -1,4 +1,22 @@
 package hotelBookingProgram;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class HotelBookingList {
+
+    private List<HotelBooking> hotelBookingList;
+
+    public void add(HotelBooking hotelBooking){
+        hotelBookingList.add(hotelBooking);
+    }
+
+    public boolean checkBooking(String tempid){
+        if(hotelBookingList.getboo)
+    }
+
+    public
+
 }
+
+
