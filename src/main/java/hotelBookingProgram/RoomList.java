6c51ed4f17ab02d2e386abeb4db84c8477877666
Shinny <siniya979@gmail.com>
2022-11-26 02:09:08 +0900

    roomNumber을 매개변수로 받아 해당 요소를 가진 인덱스의 roomPrice을 반환하는 메서드를 추가했습니다.

--- src/main/java/hotelBookingProgram/RoomList.java
+++ src/main/java/hotelBookingProgram/RoomList.java
@@ -4,16 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoomList {
+
     private List<Room> roomList = new ArrayList<>();
 
-    public RoomList() {
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
-        this.roomList.add(new Room("101호", "스탠다드 룸", 120000));
+    public RoomList(){
+        this.roomList.add(new Room("101", "스탠다드",120_000));
+        this.roomList.add(new Room("102", "더블룸(싱글배드)",220_000));
+        this.roomList.add(new Room("103", "더블룸(더블배드)",220_000));
+        this.roomList.add(new Room("104", "패밀리룸",320_000));
+        this.roomList.add(new Room("105", "스위트",420_000));
     }
+
+    // (RL-1) 룸 전체 목록 뽑아내는 메서드
+
+    // (RL-2) 룸 넘버를 입력하면 해당 방 금액을 리턴하는 메서드
+    public int getRoomPrice(String roomNumber){
+        int index = roomList.indexOf(roomNumber);
+        return roomList.get(index).getRoomPrice();
+    }
+
 }
