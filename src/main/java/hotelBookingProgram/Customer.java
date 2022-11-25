76545996ba000e7ad5a57a032cf5b85ceb0b07ee
Shinny <siniya979@gmail.com>
2022-11-26 01:57:32 +0900

    Customer 생성자를 만들었습니다.

--- src/main/java/hotelBookingProgram/Customer.java
+++ src/main/java/hotelBookingProgram/Customer.java
@@ -4,4 +4,9 @@ public class Customer {
     public String name;
     public int money;
     public String phoneNumber;
+
+    public Customer(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
 }
