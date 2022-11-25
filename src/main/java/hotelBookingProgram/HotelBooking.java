7625325a569d1b7918fd542d235b7a471f541257
Lee Songeon <qkfka1040@icloud.com>
2022-11-25 17:27:00 +0900

    클래스 재구성
    Hotelbooking 클래스에서 고객 정보 불러와 메소드 구성

--- src/main/java/hotelBookingProgram/HotelBooking.java
+++ src/main/java/hotelBookingProgram/HotelBooking.java
@@ -1,32 +1,19 @@
 package hotelBookingProgram;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class HotelBooking {
 
     public Room roomNumber;
-    public Customer name;
-    public Customer phoneNumber;
+    public Customer customer = new Customer();
     public String bookingId;
     public LocalDateTime bookingDate;
 
     public HotelBooking() {
         this.bookingDate = LocalDateTime.now() ;
         this.bookingId = UUID.randomUUID().toString();
-    }
-
-    public Room getRoomNumber() {
-        return roomNumber;
-    }
-
-    public Customer getName() {
-        return name;
-    }
-
-    public Customer getPhoneNumber() {
-        return phoneNumber;
     }
 
     public String getBookingId() {
@@ -37,4 +24,33 @@ public class HotelBooking {
         return bookingDate;
     }
 
+    public Room getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setName(String name) {
+        customer.name = name;
+    }
+
+    public void setMoney(int money) {
+        customer.money = money;
+    }
+
+    public String getName() {
+        return customer.name;
+    }
+
+    public int getMoney() {
+        return customer.money;
+    }
+
+    public boolean setPhoneNumber(String phoneNumber) { //tempPhoneNumber: 입력받은 핸드폰번호
+        String regEx = "(010)-\\d{4}-\\d{4}";
+        if(!Pattern.matches(regEx, phoneNumber))
+            return false;
+        else {
+            customer.phoneNumber = phoneNumber.replaceAll(regEx, "$1-$2-$3");
+            return true;
+        }
+    }
 }
