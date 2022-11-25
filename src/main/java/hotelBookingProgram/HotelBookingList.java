dacdc24ad120313a13b61bdc33feb491a6105fd1
Shinny <siniya979@gmail.com>
2022-11-26 02:10:05 +0900

    앞으로 구현해야 하는 메서드를 주석으로 명시해두었고, 기존 메서드를 설명하는 주석을 추가했습니다.

--- src/main/java/hotelBookingProgram/HotelBookingList.java
+++ src/main/java/hotelBookingProgram/HotelBookingList.java
@@ -2,16 +2,18 @@ package hotelBookingProgram;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class HotelBookingList {
 
     private List<HotelBooking> hotelBookingList = new ArrayList<>();
 
+
+    // 예약을 예약 리스트에 추가한다.
     public void add(HotelBooking hotelBooking){
         hotelBookingList.add(hotelBooking);
     }
 
+    // 예약번호를 입력하면 해당 예약번호를 가진 객체의 인덱스 값을 반환해준다, 없을 시 -1 반
     public int getListIndex(String bookingId){
         int tempidx = 0;
         for(int i=0; i<hotelBookingList.size(); i++){
@@ -23,14 +25,21 @@ public class HotelBookingList {
         return tempidx;
     }
 
+    // 예약번호를 입력하면 해당 예약을 리스트에서 삭제시키고 true를 반환한다.
     public boolean cancelBooking(String bookingId){
         int idx = getListIndex(bookingId);
         if(idx != -1) {
             hotelBookingList.remove(idx);
             return true;
-        }else return false;
+        }else {
+            return false;
+        }
     }
 
+//  (HB - 1 )
+//    public boolean checkBookingPossible(int stayingDate){
+//        // 매개변수 값과 같은 값을 가진 stayingDate를 갖는 객체가 리스트에 존재한다면
+//        // false를 반환하고 존재하지 않는 다면 True를 반환해라
+//    }
+
 }
-
-
