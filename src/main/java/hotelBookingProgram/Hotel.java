905d40ee4a247b73a02a268656dd6d7716c7876a
Shinny <siniya979@gmail.com>
2022-11-26 04:26:44 +0900

    정규식으로 번호 받아오는 부분 오류나서 주석처리 해둠. roomNumber를 저장받을 메서드 생성 phoneNumber저장 메서드는 프로그램 테스트를 위한 임시 메서드.

--- src/main/java/hotelBookingProgram/Hotel.java
+++ src/main/java/hotelBookingProgram/Hotel.java
@@ -22,8 +22,6 @@ public class Hotel {
         this.hotelMoeny = hotelMoeny;
     }
 
-    // 질문점 ( 중간에 잘못된 번호를 입력 받거나 결제 금액이 다르거나 할 때 메서드를 종료 시키는
-    // 방법은 무엇인가? 인터페이스에서 다시 메뉴 화면으로 돌아가야 하기 때문! System.exit(0)??  )
     // 1. 예약 받기 - 고객 객체를 생성해야함 / 예약 객체를 생성해야함 / 예약 리스트에 추가해야함.
     public void receiveBooking() {
 
@@ -35,34 +33,48 @@ public class Hotel {
         System.out.println("예약자 전화번호 입력하세요: ");
         String tempPhoneNumber = scanner.next();
 
-        // 번호가 맞는 형식으로 입력되어 있는지 확인하고 제대로 입력되면 객체에 저장한다.
-        if (!(hotelBooking.inputCustomerPhoneNumber(tempPhoneNumber))) {
-            System.out.println("잘못된 번호입니다. 다시 입력해주세요.");
-        }
+        // 에러 발생
+//        // 번호가 맞는 형식으로 입력되어 있는지 확인하고 제대로 입력되면 객체에 저장한다.
+//        if (!(hotelBooking.inputCustomerPhoneNumber(tempPhoneNumber))) {
+//            System.out.println("잘못된 번호입니다. 다시 입력해주세요.");
+//        }
+
 
         // 호텔 방 목록을 보여준다. 호텔 룸 프린트 메서드 실행 (RL-1)
-
-        System.out.println("숙박할 날짜를 입력해주세요 ex)0825: ");
-        int tempDate = scanner.nextInt();
-
-//         구현 해야 할 부분
-//        if( !(HB - 1 )메서드 ){
-//        //HB-1 메서드는 해당 날짜를 가진 예약이 있는 지 확인해준다.
-//              System.out.println("해당 날짜는 이미 예약이 완료되었습니다: ");
-//        }
+        System.out.println("============================");
+        roomList.printAllRoomList();
+        System.out.println("============================");
 
         System.out.println("원하는 방 번호를 입력하세요: ");
         String tempRoomNumber = scanner.next();
 
+        System.out.println("숙박할 날짜를 입력해주세요 ex)0825: ");
+        int tempStayingDate = scanner.nextInt();
+
+        // 기존 예약중 입력 받은 룸넘버와 숙박 날짜와 같은 예약이 있으면 -1을 반환한다.
+        int checkBookingPossible = hotelBookingList.checkBookingPossible(tempStayingDate,tempRoomNumber);
+
+        if(checkBookingPossible == -1){
+            System.out.println("해당 날짜는 이미 예약이 완료되었습니다.");
+            return;
+        }
+
         System.out.println("결제하실 금액을 입력하세요: ");
         int tempMoney = scanner.nextInt();
 
-        // 입력한 방번호에 해당하는 방의 금액을 tempRoomMoney에 저장한다.
-        int tempRoomMoeny = roomList.getRoomPrice(tempRoomNumber);
 
-        if(tempMoney == tempRoomMoeny ){
+        // 입력한 방번호에 해당하는 방의 금액을 tempRoomMoney에 저장한다.
+        int tempRoomMoney = roomList.getRoomPrice(tempRoomNumber);
+
+        if(tempMoney == tempRoomMoney ){
             // 입력 받은 값으로 고객 객체를 생성한다.
             hotelBooking.createCustomer(tempName,tempMoney);
+
+            // 예약 객체에 해당 룸넘버와 숙박 날짜를 저장한다.
+            hotelBooking.setPhoneNumber(tempPhoneNumber);
+            hotelBooking.dateTheCustomerWillStay(tempStayingDate);
+            hotelBooking.RoomNumberCustomerWillStay(tempRoomNumber);
+
             // 해당 예약을 리스트에 추가한다.
             hotelBookingList.add(hotelBooking);
             System.out.println("예약이 완료되었습니다.");
@@ -73,7 +85,7 @@ public class Hotel {
 
             // 호텔 자산에 돈을 추가한다.
             hotelMoeny +=tempMoney;
-        }else if(tempMoney < tempRoomMoeny){
+        }else if(tempMoney < tempRoomMoney){
             System.out.println("금액이 부족합니다.");
         }else System.out.println("결제 오류"); // 오류 메시지 따로 만들까요??
 
@@ -95,7 +107,6 @@ public class Hotel {
     // 3. 고객 예약 확인
 
     // 4. 호텔 예약 확인
-
 
 
 }
