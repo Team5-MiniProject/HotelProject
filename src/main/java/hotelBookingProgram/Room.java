02db188ee1a9294028ed7877f06a97bc8d258a54
jiyeJang <jjy2646@gmail.com>
2022-11-25 12:49:43 +0900

    roomNumber수정했습니다

--- src/main/java/hotelBookingProgram/Room.java
+++ src/main/java/hotelBookingProgram/Room.java
@@ -1,61 +1,42 @@
 package hotelBookingProgram;
 
+
 public class Room {
-
-    private String name;
-    private int roomPrice;
+    //고객 이름  방 가격 사이즈 날짜
+    private String roomNumber;
     private String roomSize;
-    private String bookingDate;
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private int roomPrice;
 
-    public Room (String name, int roomPrice, String roomSize){
-        this.name = name;
-        this.roomPrice = roomPrice;
+    public Room (String roomNumber, String roomSize, int roomPrice){
+        this.roomNumber = roomNumber;
         this.roomSize = roomSize;
-        this.bookingDate = formatter.format(LocalDateTime.now());
-    }
-
-    public String toString(){
-        return name +"    "+ roomSize+"    "+bookingDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getroomPrice() {
-        return roomPrice;
-    }
-
-    public void setroomPrice(int roomPrice) {
         this.roomPrice = roomPrice;
+    }
+
+    public String getRoomNumber() {
+        return  roomNumber;
+    }
+    public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber;
     }
 
     public String getroomSize() {
         return roomSize;
     }
-
     public void setroomSize(String roomSize) {
         this.roomSize = roomSize;
     }
 
-    public String getBookingDate() {
-        return bookingDate;
+    public int getroomPrice() {
+        return roomPrice;
+    }
+    public void setroomPrice(int roomPrice) {
+        this.roomPrice = roomPrice;
     }
 
-    public void setBookingDate(String BookingDate) {
-        this.bookingDate = bookingDate;
-    }
 
-    public DateTimeFormatter getFormatter() {
-        return formatter;
-    }
-
-    public void setFormatter(DateTimeFormatter formatter) {
-        this.formatter = formatter;
+    public String toString(){
+        return roomNumber +"    "+ roomSize+"    "+roomPrice;
     }
 }
+
+
