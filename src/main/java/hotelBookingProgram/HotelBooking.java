5df682a7f4e5254a70448ff653eae89bc9818f15
Lee Songeon <qkfka1040@icloud.com>
2022-11-25 15:07:49 +0900

    HotelBooking 클래스 구현

--- src/main/java/hotelBookingProgram/HotelBooking.java
+++ src/main/java/hotelBookingProgram/HotelBooking.java
@@ -1,4 +1,40 @@
 package hotelBookingProgram;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 public class HotelBooking {
+
+    public Room roomNumber;
+    public Customer name;
+    public Customer phoneNumber;
+    public String bookingId;
+    public LocalDateTime bookingDate;
+
+    public HotelBooking() {
+        this.bookingDate = LocalDateTime.now() ;
+        this.bookingId = UUID.randomUUID().toString();
+    }
+
+    public Room getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Customer getName() {
+        return name;
+    }
+
+    public Customer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public LocalDateTime getBookingDate() {
+        return bookingDate;
+    }
+
 }
