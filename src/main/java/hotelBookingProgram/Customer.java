a0fbf7cd37d0ef02ac989cc59ccc831a140f5ccd
Lee Songeon <qkfka1040@icloud.com>
2022-11-25 12:25:58 +0900

    Customer 클래스 구현

--- src/main/java/hotelBookingProgram/Customer.java
+++ src/main/java/hotelBookingProgram/Customer.java
@@ -1,8 +1,43 @@
 package hotelBookingProgram;
 
+import java.util.regex.Pattern;
+
 public class Customer {
+    public String name;
+    public int money;
+    public String phoneNumber;
+
+    public Customer(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public boolean phoneNumber(String tempPhoneNumber) { //tempPhoneNumber: 입력받은 핸드폰번호
+        String regEx = "(010)(.{4})(.{4})";
+        if(!Pattern.matches(regEx, tempPhoneNumber))
+            return false;
+        else {
+            this.phoneNumber = tempPhoneNumber.replaceAll(regEx, "$1-$2-$3");
+            return true;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 
 }
+
+
+
+
 
 
 
